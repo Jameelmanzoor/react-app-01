b9c53e56db3671f4ac44a8499a6b545f3efec67b
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-08-25 10:25:23 +0500

    Created a new component SayHello and added to App.js
    Created a new component SayHello and added to App.js inside a dive element.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SayHello from "./SayHello"
 
 function App({name, age}) {
-  return <div>Hello World from app.js updated. Created by {name}. Age = {age - 50}</div>;
+  return <div>Hello World from app.js updated. Created by {name}. Age = {age - 50}
+  <br/>
+  <SayHello firstName = {name}></SayHello>
+  </div>;
+
 }
 
 export default App;
---
+++ src/SayHello.js
@@ -0,0 +1,9 @@
+// we are creating a new React component
+
+import React from 'react';
+
+function SayHello({firstName}) {
+    return <p>This is para in sayHello function. firstName = {firstName}</p>
+}
+
+export default SayHello;
