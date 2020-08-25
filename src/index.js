2210bd55cd32ab04c29b944e92e7033c6c490745
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-08-25 08:48:10 +0500

    Added hello world to index.js
    We have some text to play with index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>Heloo World from Jameel Ahmad</div>,
   document.getElementById('root')
 );
 
