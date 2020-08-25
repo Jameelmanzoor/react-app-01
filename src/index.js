ad225410e03059ca91dfab6701b98d7d27193ce8
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-08-25 08:57:45 +0500

    updated app.js and added to app.js
    We have removed default elements from app.js and added a div with hello world. Then render that app.js componet in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <div>Heloo World from Jameel Ahmad</div>,
+  <App/>,
   document.getElementById('root')
 );
 
