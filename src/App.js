f0863506fe6847619af1bedb8e5fc181e3fa8bb5
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-08-25 10:05:12 +0500

    updated app.js and index.js. added properties
    We passed some properties from index.js in form of object{ name: value } to app.js.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return <div>Hello World from app.js</div>;
+function App({name, age}) {
+  return <div>Hello World from app.js updated. Created by {name}. Age = {age - 50}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App/>,
+  <div><App name="Jameel Ahmad" age={25}/></div>,
   document.getElementById('root')
 );
 
